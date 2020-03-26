b7041ca464d358bb108503953f05bff6c98dfaa7
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-03-26 20:41:31 +0000

    debugged the javascript file on sign-up page

--- sign-up.js
+++ sign-up.js
@@ -3,7 +3,20 @@ const signUp = document.getElementById('sign-up');
 const signInForm = document.getElementById('sign-in-container');
 const signUpForm = document.getElementById('sign-up-container');
 
-signIn.addEventListener('click', ()=> {
-    signUpForm.style.display = 'none';
-    signInForm.style.display = 'flex';
+
+console.log(signIn)
+console.log(signUp)
+console.log(signInForm)
+console.log(signUpForm)
+
+
+signIn.addEventListener('click', (event) => {
+    event.preventDefault()
+    signUpForm.style.display = "none";
+    signInForm.style.display = "block";
+});
+signUp.addEventListener('click', (event) => {
+    event.preventDefault()
+    signInForm.style.display = "none";
+    signUpForm.style.display = "block";
 });
