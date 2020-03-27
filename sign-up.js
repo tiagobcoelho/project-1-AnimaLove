7d96c07518d6f107f9412946f3abab8dbc3cff26
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-03-27 08:38:25 +0000

    fixed responsitivy of our-dogs-page added a sign-in-message to the sign-up/sign-in page

--- sign-up.js
+++ sign-up.js
@@ -2,6 +2,8 @@ const signIn = document.getElementById('sign-in');
 const signUp = document.getElementById('sign-up');
 const signInForm = document.getElementById('sign-in-container');
 const signUpForm = document.getElementById('sign-up-container');
+const signInMessage = document.getElementById('sign-in-message')
+const signUpMessage = document.getElementById('sign-up-message')
 
 
 console.log(signIn)
@@ -13,10 +15,14 @@ console.log(signUpForm)
 signIn.addEventListener('click', (event) => {
     event.preventDefault()
     signUpForm.style.display = "none";
+    signUpMessage.style.display = "none";
     signInForm.style.display = "block";
+    signInMessage.style.display = "block";
 });
 signUp.addEventListener('click', (event) => {
     event.preventDefault()
     signInForm.style.display = "none";
+    signInMessage.style.display = "none";
     signUpForm.style.display = "block";
+    signUpMessage.style.display = "block";
 });
